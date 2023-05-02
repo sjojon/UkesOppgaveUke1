25ec998f84ed563b5556bfd761270b2608b7f499
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-02 18:24:16 +0200

    Initial commit

---
+++ UkesOppgaveUke1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UkesOppgaveUke1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "! " + "Your new hobby is " + GetRandomHobby());
+        }
+
+        static string GetRandomHobby()
+        {
+            string[] hobby = { "Gaming", "Dancing", "Fishing", "Sky diving", "Tennis", "Badminton", "Hunting", "Gardening" };
+
+            Random random = new();
+            int number = random.Next(0, hobby.Length);
+            return hobby[number];
+        }
+    }
+}
